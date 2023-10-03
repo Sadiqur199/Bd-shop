349f1f84e291c8309df86c40e5d28a07a8690cb3
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-10-04 02:46:51 +0600

    filtering product

--- controller/productctrl.js
+++ controller/productctrl.js
@@ -58,14 +58,31 @@ const getaProduct = asyncHandler(async (req,res) =>{
 
 const getAllProduct = asyncHandler(async (req,res) =>{
   try{
+    //Filtering 
     const queryObj = {...req.query};
     const excludeFields = ['page', 'sort', 'limit', 'fields']
      excludeFields.forEach((el) => delete queryObj[el])
      
-     
-     
-   const getallProducts = await Product.where("category").equals(req.query.category)
-   res.json(getallProducts)
+     console.log(queryObj)
+
+     let queryStr = JSON.stringify(queryObj);
+     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+
+     let query = Product.find(JSON.parse(queryStr))
+
+
+     //Sorting
+      if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(' ')
+       query = query.sort(sortBy)
+      }
+      else{
+        query= query.sort('-createdAt')
+      }
+
+
+   const product = await query;
+   res.json(product)
   }
   catch(error){
     throw new Error(error)

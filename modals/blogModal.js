d288efdefbacd5ed7096a7129512c47f363c350b
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-10-07 05:27:04 +0600

    blog sectionn implement

---
+++ modals/blogModal.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    numViews:{
+        type:String,
+        required:true,
+    },
+});
+
+//Export the model
+module.exports = mongoose.model('Blog', blogSchema);

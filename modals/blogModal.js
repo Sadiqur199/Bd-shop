aa4f336d3d289ab4383e6997ed7ff8395443e18c
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-10-07 05:37:06 +0600

    create a blog schema

--- modals/blogModal.js
+++ modals/blogModal.js
@@ -15,9 +15,45 @@ var blogSchema = new mongoose.Schema({
         required:true,
     },
     numViews:{
-        type:String,
-        required:true,
+        type:Number,
+        default: 0,
     },
+    isLiked:{
+        type:Boolean,
+        default:false,
+    },
+    isDisLiked:{
+      type:Boolean,
+      default:false,
+    },
+    likes:[
+      {
+      type:mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+    dislikes:[
+      {
+      type:mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  image:{
+    type:String,
+    default:"https://img.freepik.com/premium-photo/how-start-blog-blogging-beginners-ways-monetize-your-blog-blog-word-table-with-laptop_72482-5630.jpg",
+  },
+  author:{
+    type:String,
+    default:"Admin",
+  }
+},{
+  toJSON: {
+    virtuals:true,
+  },
+  toObject:{
+    virtuals:true,
+  },
+  timestamps:true,
 });
 
 //Export the model

de48543af6f69a0945844726ba12dee8e65e4216
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-10-09 02:42:08 +0600

    implement get a blog and update a views

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -29,5 +29,21 @@ const updateBlog = asyncHandler(async (req,res)=>{
 });
 
 
+const getBlog = asyncHandler(async (req,res)=>{
+  const {id} = req.params;
+ try{
+   const getBlog = await Blog.findById(id);
+  const updateViews = await Blog.findByIdAndUpdate(id,{
+    $inc:{ numViews: 1 }
+   },
+   {
+    new:true
+  });
+   res.json(updateViews)
+ }
+ catch(error){
+   throw new Error(error)
+ }
+});
 
-module.exports = {createBlog,updateBlog}
+module.exports = {createBlog,updateBlog,getBlog}
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { authMiddleware, isAdmin } = require("../middleware/AuthMiddleware");
-const { createBlog, updateBlog } = require("../controller/blogCtrl");
+const { createBlog, updateBlog, getBlog } = require("../controller/blogCtrl");
 const router = express.Router();
 
 router.post('/',authMiddleware,isAdmin,createBlog)
 router.put('/:id',authMiddleware,isAdmin,updateBlog)
+router.get('/:id',getBlog)
 
 module.exports = router;

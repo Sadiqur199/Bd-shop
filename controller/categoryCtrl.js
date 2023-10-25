c771dd1853aaa52a67bba20d3e31988b57b338e8
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-10-26 02:28:05 +0600

    implement category controler

--- controller/categoryCtrl.js
+++ controller/categoryCtrl.js
@@ -2,4 +2,14 @@ const Category = require("../modals/categoryModal")
 const asyncHandler = require("express-async-handler")
 const validateMongoDbId = require('../utills/validateMongodbid');
 
+const createCategory = asyncHandler(async(req,res) =>{
+  try{
 
+  }
+  catch(error){
+    throw new Error(error)
+  }
+})
+
+
+module.exports = {}

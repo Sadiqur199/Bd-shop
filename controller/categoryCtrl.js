d185d33f20f4f779bc50e055992b757117b62d6b
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-10-29 00:54:13 +0600

    implement update category

--- controller/categoryCtrl.js
+++ controller/categoryCtrl.js
@@ -12,5 +12,15 @@ const createCategory = asyncHandler(async(req,res) =>{
   }
 })
 
+const updateCategory = asyncHandler(async(req,res) =>{
+  try{
+   const newCategory = await Category.create(req.body);
+   res.json(newCategory)
+  }
+  catch(error){
+    throw new Error(error)
+  }
+})
 
-module.exports = {createCategory}
+
+module.exports = {createCategory,updateCategory}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const { createCategory } = require("../controller/categoryCtrl")
+const { authMiddleware, isAdmin } = require("../middleware/AuthMiddleware")
 const router = express.Router()
 
 
-router.post("/",createCategory)
+router.post("/",authMiddleware,isAdmin, createCategory)
 
 module.exports = router
